309e7b2ed1c2e23ba464da12e07a70e2ec4ac956
Kirill Kornilov <k-korni1ov@yandex.ru>
2023-12-24 18:31:47 +0400

    fear: removeKFromList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let head = l;
+  let node = head;
+  let prevNode = l;
+  while (node.next != null) {
+    if (node.value === k) {
+      if (node === head) {
+        console.log("node===head");
+        head = node.next;
+        prevNode = head;
+      } else {
+        prevNode.next = node.next;
+      }
+    } else {
+      prevNode = node;
+    }
+    node = node.next;
+  }
+  if (node.value === k) {
+    prevNode.next = null;
+  }
+
+  return head;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
